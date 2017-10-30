5df247a1179beecc933d90e9c0d6e7929bb2ad87
Adrian Becerril <abc88@alumnos.unican.es>
2017-10-30 10:29:28 +0100

    Se ha generado la documentación del código de la aplicación, se ha reducido el tiempo de muestra del SplashScreen, se ha modificado la reacción de la aplicación cuando se reinicia, se ha mejorado la interfaz gráfica y se ha limpiado el código.
    Adrián

--- src/Mover/app/src/main/java/com/air/mover/Presenter/ListLineasPresenter.java
+++ src/Mover/app/src/main/java/com/air/mover/Presenter/ListLineasPresenter.java
@@ -10,27 +10,34 @@ import android.widget.Toast;
 import com.air.mover.DAO.DataLoader.ParserJSON;
 import com.air.mover.DAO.DataLoader.RemoteFetch;
 import com.air.mover.DAO.Model.Linea;
+import com.air.mover.R;
 import com.air.mover.View.IListLineasView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by alejandro on 11/10/17.
+ * Esta clase forma el enlace entre el modelo de datos de lineas de TUS y
+ * la vista correspondiente a las lineas de TUS de la aplicacion
+ *
+ * @version 30/10/17
  */
-
-
-
 
 public class ListLineasPresenter
 {
-    private IListLineasView listLineasView;
+    private IListLineasView listLineasView; //Vista de las lineas de TUS
     private List<Linea> listaLineasBus;
-    private RemoteFetch remoteFetchLineas;
+    private RemoteFetch remoteFetchLineas; //Modelo de datos lineas de TUS
     private Context context;
 
 
-
-
+    /**
+     * Metodo constructor que inicializa los atributos de
+     * la clase
+     *
+     * @param context contexto de la aplicacion
+     * @param listLineasView vista donde se mostraran las lineas del TUS
+     */
     public ListLineasPresenter(Context context, IListLineasView listLineasView)
     {
         this.listLineasView = listLineasView;
@@ -39,33 +46,59 @@ public class ListLineasPresenter
     }// ListLineasPresenter
 
 
-
+    /**
+     * Clase asincrona que permite descargar las lineas de TUS de internet
+     * asi como de almacenarlas. Tambien se encarga de la gestion de la vista
+     * durante el proceso entero de descarga y visualizacion
+     */
     private class LeerLineasInternet extends AsyncTask<Object, Boolean, Boolean>
     {
+        /**
+         * Metodo que se ejecuta antes de iniciar el proceso en el mismo
+         * hilo de ejecucion que la aplicacion
+         */
         @Override
         protected void onPreExecute()
         {
             listLineasView.showProgress(true);
-        }
+        }//onPreExecute
 
+        /**
+         * Metodo que ejecuta la tarea en un hilo diferente al de
+         * ejecucion con el fin de no bloquear la interfaz
+         *
+         * @param l
+         * @return result
+         */
         @Override
         protected Boolean doInBackground(Object... l)
         {
             obtenLineas();
             return true;
-        }
+        }//doInBackground
 
+        /**
+         * Metodo que se ejecuta cuando finaliza el metodo doInBackground
+         * y recibe el resultado de dicho metodo para tratarlo y actualizar
+         * la interfaz de usuario en consecuencia
+         *
+         * @param result resultado del metodo doInBackground
+         */
         @Override
         protected void onPostExecute(Boolean result)
         {
             if (result) {
                 listLineasView.showList(getListaLineasBus());
                 listLineasView.showProgress(false);
-            }
-        }
+            }//if
+        }//onPostExecute
     }
 
 
+    /**
+     *  Metodo que se encarga de poner en marcha el presenter
+     *  realizando el enlace entre el modelo y la vista
+     */
     public void start()
     {
         //Procedemos a la lectura de la informacion sobre las lineas proporcionadas por el ayuntamiento
@@ -83,15 +116,17 @@ public class ListLineasPresenter
      * @return
      */
     public boolean obtenLineas(){
-        try {
+        try
+        {
             remoteFetchLineas.getJSON(RemoteFetch.URL_LINEAS_BUS);;
             listaLineasBus = ParserJSON.readArrayLineasBus(remoteFetchLineas.getBufferedData());
             return true;
-        }catch(Exception e){
+        }//try
+        catch(Exception e){
             Log.e("ERROR","Error en la obtención de las lineas de Bus: "+e.getMessage());
             e.printStackTrace();
             return false;
-        }//try
+        }//catch
     }//obtenLineas
 
 
@@ -109,14 +144,18 @@ public class ListLineasPresenter
         String textoLineas="";
         if(listaLineasBus!=null)
         {
-            for (int i=0; i<listaLineasBus.size(); i++){
+            for (int i=0; i<listaLineasBus.size(); i++)
+            {
                 textoLineas=textoLineas+listaLineasBus.get(i).getIdentifier()+"\n\n";
             }//for
-        }else{
-            textoLineas="Sin lineas";
         }//if
+        else
+        {
+            textoLineas="Sin lineas";
+        }//else
         return textoLineas;
     }//getTextoLineas
+
 
 }// ListLineasPresenter
 
