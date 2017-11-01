082b430913937bc65bd873747438933e584cab3c
Alvaro <alvaroibrain@hotmail.com>
2017-11-01 23:52:49 +0100

    Corregidos errores de calidad interna para conseguir la calificacion de 3 A.

---
+++ src/Mover/app/src/main/java/com/air/mover/view/IListLineasView.java
@@ -0,0 +1,14 @@
+package com.air.mover.view;
+
+import com.air.mover.dao.Model.Linea;
+
+import java.util.List;
+
+/**
+ * Interfaz que implementará la vista de una lista de lineas de TUS
+ * @version 29/10/17
+ */
+public interface IListLineasView {
+    void showList(List<Linea> lineaList);
+    void showProgress(boolean state);
+}//IListLineasView
